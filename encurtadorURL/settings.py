c528a6e4e12ad930da6a040a68bb0c155b4934a8
= <mmps@cin.ufpe.br>
2017-09-30 20:49:23 -0300

    update settings.py

--- encurtadorURL/settings.py
+++ encurtadorURL/settings.py
@@ -119,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
